9fa41a4fafb52ee659778182a7bfa7a0c18a718a
Ruben Requena <rubnrequena@gmail.com>
2018-02-07 04:13:54 -0400

    correccion de ip a falta de cookie

--- usuario/js/vars.js
+++ usuario/js/vars.js
@@ -5,9 +5,9 @@
 // SOCKET
 var loc = location.href.split("/")[3];
 if (loc=="LotoVictoria") loc = "127.0.0.1:4023";
-if (loc=="animales") loc = "192.111.37.10:4003";
-else if (loc=="animal") loc = "192.111.37.107:4013";
-else if (loc=="animalitos") loc = "192.111.37.107:4023";
+if (loc=="animales") loc = "104.129.171.16:4003";
+else if (loc=="animal") loc = "104.129.171.162:4013";
+else if (loc=="animalitos") loc = "104.129.171.162:4023";
 var host = $.cookie("usuario") || loc;
 
 var socket = new Net("ws://"+host,false);
