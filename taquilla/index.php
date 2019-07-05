36787cf117c47636abdc1c0dc147ec650e29abf1
Ruben Requena <rubnrequena@gmail.com>
2019-07-05 00:03:15 -0400

    ruta alternativa

--- taquilla/index.php
+++ taquilla/index.php
@@ -1,11 +1,11 @@
 <?php	
 	require('../redir.php');
 	setcookie("taquilla",$port["taquilla"]);
-
 	date_default_timezone_set('America/Caracas');
     $file = "index.html";
     $data = file_get_contents($file);
     $data = str_replace('@version',filemtime($file),$data);
     $data = str_replace('@verStamp',date('ymd',filemtime($file)),$data);
+    $data = str_replace('@rutaAlt','ed5f75ec.ngrok.io',$data);
     echo $data;
 ?>
